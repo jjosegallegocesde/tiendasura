939da8566fe7a6e8dd96da7d234e25516e85efe1
Elizabethzapatayepes <eli-2304@hotmail.com>
2022-07-26 17:34:21 -0500

    se crean ejemplos de DOM

---
+++ src/controllers/aprendiendoDOM.js
@@ -0,0 +1,12 @@
+//PASOS PARA CONTROLAR UNA ETIQUETA DESDE JS
+
+//1. CONTROLANDO EL CONTENIDO
+let etiquetaTitulo=document.getElementById("titulo")
+let etiquetaImagen=document.getElementById("foto")
+
+
+//1.1 MANIPULANDO EL TEXTO DE LA ETIQUETA(ELEMENTO)
+etiquetaTitulo.textContent="jefe usted es como mi padre"
+
+//1.2 MANIPULANDO EL SRC
+etiquetaImagen.src="https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/gohan1.webp?alt=media&token=ce45c3f8-146b-41c9-81cc-08441e6ed6b4"
--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,5 +1,5 @@
 //CREANDO VARIABLES
-let nombreUsuario="Juan Jose"
+/*let nombreUsuario="Juan Jose"
 const EDAD_USUARIO=33
 let estaturaUsuario=1.62
 let hinchaDelVerde=true
@@ -7,4 +7,59 @@ let hinchaDelVerde=true
 console.log(nombreUsuario)
 
 
-console.log(EDAD_USUARIO)
+console.log(EDAD_USUARIO)*/
+
+//MERCANDO CON EL PROFE
+let producto1="arroz"
+let producto2="leche"
+let producto3="huevo"
+let producto4="carne de cerdo"
+let producto5="pollo"
+let producto6="galletas"
+let producto7="panela"
+let producto8="cafe"
+let producto9="tapa roja"
+let producto10="cerveza"
+let producto11="tomate"
+let producto12="papas"
+let producto13="lechuga"
+let producto14="frijoles"
+let producto15="aceite"
+
+//ARREGLOS DE DATOS
+//UN ARREGLO ES UNA VARIABLE ESPECIAL
+//1. ARREGLO TRADICIONAL
+let numeros=[1,2,8,9,45]
+let nombres=["juan","catalina","mario","carlos"]
+let contagios=[true,false,false,false,true]
+//1.1 accediendo a todos los datos(elementos) de un arreglo
+console.log(numeros)
+//1.2 accediendo A UN SOLO ELEMENTO DEL ARREGLO
+console.log(numeros[3])
+
+//2. ARREGLOS ORIENTADOS A OBJETOS ARREGLOS POO
+let notas=Array(3.5,2.8,3.9,5)
+console.log(notas)
+console.log(notas[1])
+
+//3. VARIABLES ESPECIALES PARA GUARDAR VARIOS DATOS
+//DE TIPOS DIFERENTES
+//OBJETOS
+let persona={
+    nombre:"Juan Jose",
+    apellidos:["gallego","mesa"],
+    edad:33,
+    hinchaDelMejor:true
+}
+//3.1 accediendo a todos los datos de un objeto
+console.log(persona)
+//3.2 Accediendo a un solo atributo del objeto
+console.log(persona.nombre)
+console.log(persona.apellidos[0])
+
+
+
+
+
+
+
