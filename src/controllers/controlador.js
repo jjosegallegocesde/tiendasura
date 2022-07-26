7fb7a91ef617b38a87c74eb7047156efc4b3151e
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-07-26 10:39:29 -0500

    ok

---
+++ src/controllers/controlador.js
@@ -0,0 +1,10 @@
+//CREANDO VARIABLES
+let nombreUsuario="Juan Jose"
+const EDAD_USUARIO=33
+let estaturaUsuario=1.62
+let hinchaDelVerde=true
+
+console.log(nombreUsuario)
+
+
+console.log(EDAD_USUARIO)
