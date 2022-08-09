2bcf99c6374fb872de8c81ea26bdc9e146f7daf2
Elizabethzapatayepes <eli-2304@hotmail.com>
2022-08-09 17:25:37 -0500

    ok

--- src/controllers/ampliarInformacion.js
+++ src/controllers/ampliarInformacion.js
@@ -1,10 +1,18 @@
 let contenedorProductos=document.getElementById("fila")
 
 //escucho clic en la fila
+
+let informacionProducto={}
 contenedorProductos.addEventListener("click",function(evento){
     if(evento.target.classList.contains("img-fluid")){
-        console.log(evento.target.parentElement.querySelector("h3").textContent)
-        console.log(evento.target.parentElement.querySelector("h2").textContent)
+        informacionProducto.nombre=(evento.target.parentElement.querySelector("h3").textContent)
+        informacionProducto.precio=(evento.target.parentElement.querySelector("h2").textContent)
+        informacionProducto.descripcion=(evento.target.parentElement.querySelector("p").textContent)
+
+        //guardando un objeto en memoria
+        localStorage.setItem(JSON.stringify("producto",informacionProducto))
+
+        window.location.href="./ampliarInfo.html"
 
     }
   
---
+++ src/controllers/detalleProducto.js
@@ -0,0 +1,5 @@
+console.log("hola estoy detallando la compra")
+
+//llamar a la memoria
+let producto=localStorage.getItem("producto")
+console.log(producto)
